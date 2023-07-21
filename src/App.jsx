630cc14def75366cd1ffa5944ce1de943304b741
Ajarek <ajarek@poczta.onet.pl>
2023-07-22 00:04:35 +0200

    added handleButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 import Section from './components/Section/Section'
 import data from './assets/data.json'
+import { useEffect, useState } from 'react'
 const App = () => {
+  const [newData, setNewData]=useState(data)
+  const handleButton=(id)=>{
+     const item =newData.find(el=>el.id===id)
+     
+  }
   return (
     <div>
       <div className='app-wrapper'>
         <h1>Accordion Title Here</h1>
-        {data.map((el) => {
+        {newData.map((el) => {
           return (
             <Section
               key={el.id}
               sectionTitle={el.title}
-              sectionText={el.text}
-              icon={'✔️'}
+              sectionText={el.active?el.text:null}
+              icon={el.active?'✔️':'❌'}
+              onClick={()=>handleButton(el.id)}
             />
           )
         })}
